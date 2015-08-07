2cd1eb0ca0f36ea9d37118f1f41252d98a5fa7c3
Samu Juvonen <samu.juvonen@gmail.com>
2015-08-07 13:51:37 +0300

    backup

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var app = express();
 var config = require("./config");
-var elastic = require("./lib/elastic");
 var encoders = require("./lib/encoders");
-
-var searcher = require("./lib/searcher");
+var controller = require("./lib/controller");
 
 app.listen(config.port, config.address, function() {
   console.log("Listening");
@@ -21,44 +19,19 @@ app.get("/", function(req, res) {
   res.send("API INDEX");
 });
 
-app.get("/v3/:type", function(req, res, next) {
-  console.log("SEARCH TYPE", req.params.type);
+app.get("/v3/:type", controller.list);
+app.get("/v3/:type/:id", controller.fetch);
 
-  searcher.create(req.params.type).search(req.query).then(function(result) {
-    res.result = result;
-    next();
-  }, function(error) {
-    res.send("error");
-    console.error("ERROR", error);
-  });
-});
+app.use(encoders.selectEncoder);
+app.use(encoders.encodeResponse);
 
-app.get("/v3/:type/:id", function(req, res, next) {
-  searcher.create(req.params.type).search({id: req.params.id}).then(function(result) {
-    res.result = result.hits[0];
-    next();
-  }, function(error) {
-    res.send("error");
-    console.error("ERROR", error);
-  });
-});
-
+/**
+ * Final handler for successful requests. Will write the response to the client.
+ * If no data is present, will pass to the next middleware which prints and error.
+ */
 app.use(function(req, res, next) {
-  if (res.result) {
-    if (req.accepts("json")) {
-      res.data = res.result;
-      res.type("json");
-    } else if (req.accepts("xml")) {
-      res.data = "<data><list><row>First</row><row>Second</row></list></data>";
-      res.type("xml");
-    }
-  }
-  next();
-});
-
-app.use(function(req, res, next) {
-  if (res.data) {
-    res.send(res.data);
+  if ("data" in res.locals) {
+    res.send(res.locals.data);
   } else {
     next();
   }
