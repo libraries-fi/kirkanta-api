e43c4d8f267796108eba73a863ea0b9bac44db6a
Samu Juvonen <samu.juvonen@gmail.com>
2015-09-03 21:41:41 +0300

    misc

--- doc/utils/compile.sh
+++ doc/utils/compile.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-/usr/lib/ruby/gems/2.0.0/gems/github-markdown-0.5.3/bin/gfm --readme ../documentation.md > body.html && cat header.html body.html footer.html > ../../../public/documentation.html
-
+gfm --readme ../documentation.md > body.html && cat header.html body.html footer.html > ../../public/documentation.html
